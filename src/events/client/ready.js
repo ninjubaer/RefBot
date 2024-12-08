2ad6ef3bb8e35e0d0976a9fee8f7e42017cc33af
ninju <74989292+ninjubaer@users.noreply.github.com>
2024-12-08 13:09:55 +0100

    added custom activity

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,9 @@
+const { ActivityType } = require('discord.js');
 module.exports = {
     name: 'ready',
     async execute(client) {
         console.log(`Logged in as ${client.user.tag}`);
+        client.user.setActivity({ name: 'supporting a dictatorship', type: ActivityType.Custom });
         require('../../commands/index.js').init(client);
     }
 }
