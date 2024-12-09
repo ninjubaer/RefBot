50997f77330a3d065039d7dad7baf6c877b9d0b5
ninju <74989292+ninjubaer@users.noreply.github.com>
2024-12-09 09:40:56 +0100

    new /level card

--- src/commands/level/level.js
+++ src/commands/level/level.js
@@ -1,5 +1,21 @@
 const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 const { RankCardBuilder, Font } = require('canvacord');
+const { createCanvas, loadImage, registerFont, CanvasRenderingContext2D } = require('canvas');
+CanvasRenderingContext2D.prototype.roundRect = function (x, y, width, height, radius) {
+    if (width < radius) width = radius * 2;
+    this.beginPath();
+    this.moveTo(x + radius, y);
+    this.arcTo(x + width, y, x + width, y + height, radius);
+    this.arcTo(x + width, y + height, x, y + height, radius);
+    this.arcTo(x, y + height, x, y, radius);
+    this.arcTo(x, y, x + width, y, radius);
+    this.closePath();
+    return this;
+}
+const path = require('path');
+registerFont(path.join(__dirname, './../../assets/font/Roboto-Bold.ttf'), { family: 'Roboto', weight: 'Bold' });
+let background;
+(async ()=>{background = await loadImage(path.join(__dirname, './../../assets/images/levelcard.png'))})();
 const levelFunctions = require('../../utils/levels');
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,7 +30,7 @@ module.exports = {
         const user = await require('../../db/index').fetchUser(mongoclient, target.id);
         const level = levelFunctions.getLevel(user.xp);
         const rank = await levelFunctions.getRank(user.xp, mongoclient) + 1;
-        const rankCard = new RankCardBuilder()
+       /*  const rankCard = new RankCardBuilder()
             .setDisplayName(target.globalName)
             .setUsername(target.username)
             .setAvatar(target.displayAvatarURL({ format: 'png', size: 512 }))
@@ -26,8 +42,51 @@ module.exports = {
         const img = await rankCard.build({
             fonts: [font],
             format: 'png'
-        })
-        const attachment = new AttachmentBuilder(img, { name: 'levelcard.png' });
+        }) */
+        const rankCard = await createRankCard(target.username, target.displayAvatarURL({ extension: 'png', size: 512}), level, rank, user.xp, levelFunctions.xpForNextLevel(level));
+        const attachment = new AttachmentBuilder(rankCard, { name: 'levelcard.png' });
         await interaction.editReply({ files: [attachment] });
     }
+}
+
+
+async function createRankCard(username, avatar, level, rank, xp, requiredXP) {
+    const canva = createCanvas(480, 607);
+    const ctx = canva.getContext('2d');
+    background = await background;
+    ctx.drawImage(background, 0, 0, canva.width, canva.height);
+
+    const avatarImage = await loadImage(avatar);
+
+    // draw avatar
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(240, 196, 118, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
+    ctx.drawImage(avatarImage, 122, 78, 236, 236);
+    ctx.restore();
+    // draw username
+    ctx.font = 'Bold 40px "Roboto"';
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = 'center';
+    ctx.fillText(username, canva.width/2, 370, canva.width - 40);
+
+    // draw level
+    ctx.font = 'Bold 30px "Roboto"';
+    ctx.fillText(level, 112, 509);
+
+    // draw rank
+    ctx.fillText('#' + rank, 370, 509);
+
+    // draw xp
+    ctx.font = 'Bold 20px "Roboto"';
+    ctx.fillText(`${xp} / ${requiredXP}`, 240, 444, 360);
+
+    // draw progress
+    ctx.fillStyle = '#fe95fe';
+    ctx.roundRect(60, 391, 360 / requiredXP * xp, 30, 15);
+    ctx.fill();
+
+    return canva.toBuffer("image/png");
 }
