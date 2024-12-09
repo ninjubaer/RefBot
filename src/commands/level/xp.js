203763a08f10a4f9f5af3b35f9ea28e23dfa5210
ninju <74989292+ninjubaer@users.noreply.github.com>
2024-12-09 22:35:23 +0100

    added xp command

---
+++ src/commands/level/xp.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { execute } = require('../../events/guild/messageCreate')
+const dbfunctions = require('../../db/index')
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('xp')
+        .setDescription('interact with xp system')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('send')
+                .setDescription('send xp to a user')
+                .addUserOption(option =>
+                    option.setName('user')
+                        .setDescription('the user to send xp to')
+                        .setRequired(true)
+                )
+                .addIntegerOption(option =>
+                    option.setName('amount')
+                        .setDescription('the amount of xp to send')
+                        .setRequired(true)
+                )
+        ),
+    async execute(interaction,client,mongoclient) {
+        switch (interaction.options.getSubcommand()) {
+            case "send":
+                const user = interaction.options.getUser('user')
+                const amount = interaction.options.getInteger('amount')
+                const sender = interaction.user
+                const senderUser = await dbfunctions.fetchUser(mongoclient, sender.id)
+                if (senderUser.xp < amount) {
+                    return interaction.reply({content: "You do not have enough xp to send that amount", ephemeral: true})
+                }
+                if (amount < 0) {
+                    return interaction.reply({content: "You cannot send negative xp", ephemeral: true})
+                }
+                const receiverUser = await dbfunctions.fetchUser(mongoclient, user.id)
+                receiverUser.xp += amount
+                senderUser.xp -= amount
+                await mongoclient.db("RefBot").collection("users").updateOne({ id: user.id }, { $set: { xp: receiverUser.xp } });
+                await mongoclient.db("RefBot").collection("users").updateOne({ id: sender.id }, { $set: { xp: senderUser.xp } });
+                return interaction.reply({embeds: [new EmbedBuilder().setTitle("XP Sent").setDescription(`Sent ${amount} xp to ${user.username}`).setAuthor({name: sender.username, iconURL: sender.displayAvatarURL()}).setColor(0x2b2d31)]})
+                break;
+            default:
+                return interaction.reply({content: "Invalid subcommand", ephemeral: true})
+                break;
+        }
+    }
+}
