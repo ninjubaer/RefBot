d71df6c057110b852b21bfac779a4c4c57135fc0
ninju <74989292+ninjubaer@users.noreply.github.com>
2024-12-09 17:57:39 +0100

    added level roles

--- src/events/guild/messageCreate.js
+++ src/events/guild/messageCreate.js
@@ -13,6 +13,29 @@ module.exports = {
         // 15-35 xp
         const xpForNextLevel = levelFunctions.xpForNextLevel(levelFunctions.getLevel(user.xp));
         user.xp += Math.floor(Math.random() * 21) + 15
+        const roleRewards = await mongoclient.db("RefBot").collection("roles").find({}).toArray();
+        let roleObj;
+        if (roleObj = roleRewards.find(role => role.requirements <= levelFunctions.getLevel(user.xp) && ((role.limit > role.given) || role.limit == 0))) {
+            let levelRole;
+            if (roleObj.id) {
+                levelRole = message.guild.roles.cache.get(roleObj.id);
+            }
+            else {
+                // create role
+                levelRole = await message.guild.roles.create({
+                    name: `${roleObj.name}`,
+                    color: 0,
+                    permissions: [],
+                    mentionable: false
+                });
+                await mongoclient.db("RefBot").collection("roles").updateOne({ _id: roleObj._id }, { $set: { id: levelRole.id } });
+            }
+            const member = message.guild.members.cache.get(message.author.id);
+            if (member && levelRole) {
+                await member.roles.add(levelRole);
+                await mongoclient.db("RefBot").collection("roles").updateOne({ _id: roleObj._id }, { $set: { given: roleObj.given + 1 } });
+            }
+        }
         if (user.xp >= xpForNextLevel) {
             message.guild.channels.cache.find(channel => channel.name === 'level-ups').send(`Congratulations ${message.author}! You have leveled up to level ${levelFunctions.getLevel(user.xp)}!`);
         }
