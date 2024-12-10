9bba6ffe8d11b2f4ccfe7d301df051776157e830
ninju <74989292+ninjubaer@users.noreply.github.com>
2024-12-10 11:42:20 +0100

    added GAMBLING

---
+++ src/commands/gambling/doubleornothing.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('discord.js')
+const dbfunctions = require('../../db/index')
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("doubleornothing")
+        .setDescription("Double or nothing!")
+        .addIntegerOption(option=>
+            option.setName("amount")
+            .setDescription("The amount of xp to bet")
+            .setRequired(true)
+        )
+        ,
+    async execute(interaction, client, mongoclient) {
+        await interaction.deferReply({fetchReply: true})
+        const user = await dbfunctions.fetchUser(mongoclient, interaction.user.id)
+        const amount = interaction.options.getInteger('amount')
+        if (amount < 0) {
+            return interaction.editReply({content: "You cannot bet negative xp!"})
+        }
+        if (amount > user.xp) {
+            return interaction.editReply({content: "You do not have enough xp to bet that amount!"})
+        }
+        const result = Math.random() >= 0.85
+        if (result) {
+            user.xp += amount
+            await mongoclient.db("RefBot").collection("users").updateOne({ id: interaction.user.id }, { $set: { xp: user.xp } });
+            return interaction.editReply({content: `You won ${amount} xp!`})
+        }
+        else {
+            user.xp -= amount
+            await mongoclient.db("RefBot").collection("users").updateOne({ id: interaction.user.id }, { $set: { xp: user.xp } });
+            return interaction.editReply({content: `You lost ${amount} xp!`})
+        }
+    }
+}
