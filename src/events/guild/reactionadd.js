936b8283c67cc8ad9beef3fdab8e4f2837512e64
ninju <74989292+ninjubaer@users.noreply.github.com>
2024-12-18 10:43:06 +0100

    added regional_indicator remover

--- src/events/guild/reactionadd.js
+++ src/events/guild/reactionadd.js
@@ -1,15 +1,25 @@
 module.exports = {
-    name: 'RAW',
+    name: 'raw',
     once: false,
-    async execute(client, data) {
+    async execute(data, _, client) {
         const { t: event, d: payload } = data;
         if (event === 'MESSAGE_REACTION_ADD') {
             const { channel_id, message_id, user_id, emoji } = payload;
             const channel = await client.channels.fetch(channel_id);
             const message = await channel.messages.fetch(message_id);
-            const user = await client.users.fetch(user_id);
-            const member = await message.guild.members.fetch(user_id);
-            client.emit('reactionAdd', message, member, emoji);
+            // check if emoji is a regional indicator
+            if (isRegionalIndicator(getEmojiCode(emoji.name))) {
+                // remove reaction
+                message.reactions.cache.get(emoji.name).users.remove(user_id);
+            }
+            
         }
     }
+}
+function getEmojiCode(emoji) {
+    return Array.from(emoji).map(char => char.codePointAt(0).toString(16)).join('-');
+}
+function isRegionalIndicator(emojiCode) {
+    const regionalIndicatorRange = /^1f1e[6-9a-f]|1f1f[0-9a-f]$/;
+    return regionalIndicatorRange.test(emojiCode);
 }
