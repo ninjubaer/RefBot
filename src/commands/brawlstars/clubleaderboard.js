4c795ed1b6e8087372463ff3d4d21307baa1f36b
ninju <74989292+ninjubaer@users.noreply.github.com>
2024-12-19 11:30:38 +0100

    added brawlsars stuff

---
+++ src/commands/brawlstars/clubleaderboard.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { BrawlStars } = require('../../utils/brawlstars')
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('brawlstarsleaderboard')
+        .setDescription('Get the leaderboard of our Brawl Stars club'),
+    async execute(interaction) {
+        interaction.deferReply({fetchReply: true})
+        const club = await BrawlStars.getClub('2CULYL0CJ')
+        console.log(club);
+        const members = club.members.map(member => {
+            return `${member.name} - ${member.trophies}`
+        })
+        interaction.editReply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle(club.name)
+                    .setDescription(members.join('\n'))
+                    .setColor('RANDOM')
+                    .setTimestamp()
+                    .setFooter('Brawl Stars Club Leaderboard')
+            ]
+        })
+    }
+}
